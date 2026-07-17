1d82dfe92ef92d8b6cea7a63f5138eacfb780df6
Justin <dfmvp2011@gmail.com>
2026-07-17 09:20:52 -0400

    Initial Commit

---
+++ Bored Mini Games/Bored_Mini_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bored_Mini_GamesApp.swift
+//  Bored Mini Games
+//
+//  Created by Justin Vanzura on 7/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Bored_Mini_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bored Mini Games/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bored Mini Games
+//
+//  Created by Justin Vanzura on 7/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
